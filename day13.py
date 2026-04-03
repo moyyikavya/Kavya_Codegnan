cf6c3c08ad67f53f67bd260104d536fb97518b78
moyyikavya <moyyikavya593@gmail.com>
2026-04-03 13:05:38 +0530

    day13 of my python fellstack journey

---
+++ day13.py
@@ -0,0 +1,86 @@
+'''#
+num=int(input("enter the limit   :"))
+for j in range(num):
+    for i in range(1,j):
+        print("i",end = " ")
+    print()
+#--------------------------------------------------------------------------------------------->   
+#square
+num=int(input("enter the limit   :"))
+for j in range(num):
+    for i in range(num):
+            print("*",end="")
+    print()
+#uotpu:enter the limit   :5
+#    *****
+#    *****
+#    *****
+#    *****
+#    *****    
+#-------------------------------------------------------------------------------------------->
+num=int(input("enter the limit   :"))       
+for j in range(num):
+    for i in range(num-j):
+        print("*",end = " ")
+    print()
+#output:enter the limit   :5
+#   * * * * * 
+#   * * * * 
+#   * * * 
+#   * * 
+#   *     
+#--------------------------------------------------------------------------------------------->   
+num=int(input("enter the limit   :"))
+for j in range(num):
+    print(" "*(num-j),end="")
+    for i in range(j+1):
+        print("*",end=" ")
+    print()    
+#OUTPUT:
+#enter the limit   :5
+#     * 
+#    * * 
+#   * * * 
+#  * * * * 
+# * * * * * 
+#--------------------------------------------------------------------------------------------->'''
+num=int(input("enter the limit   :"))
+for j in range(num):
+    print(" "*(num-j),end="")
+    for i in range(j+1):
+        print("*",end=" ")  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                   
